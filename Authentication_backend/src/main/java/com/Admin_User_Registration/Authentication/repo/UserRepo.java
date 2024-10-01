66d464ff9609d3c03dd7a6a47eff7c2c1eaf28c7
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2024-10-01 10:42:55 +0530

    UserRepo create

---
+++ Authentication_backend/src/main/java/com/Admin_User_Registration/Authentication/repo/UserRepo.java
@@ -0,0 +1,8 @@
+package com.Admin_User_Registration.Authentication.repo;
+
+import com.Admin_User_Registration.Authentication.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User,Integer> {
+
+}
