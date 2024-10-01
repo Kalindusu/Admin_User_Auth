b774d272d5953a6262dac1e230182cc2422a21ba
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2024-10-01 23:02:26 +0530

    commponent create

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Login from './Components/Login';
+import Register from './Components/Register';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1>header</h1>
+   <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />}></Route>
+      <Route path="/register" element={<Register />}></Route>
+    </Routes>
+    </BrowserRouter>
     </div>
   );
 }
--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import "./Login.css";
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className="center-form">
+      <h1>Login From Here </h1>
+      <form>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Control
+            type="password"
+            placeholder="Enter Password"
+            name="password"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Control
+            type="email"
+            placeholder="Enter Email"
+            name="email"
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">Login</Button>
+      </form>
+    </div>
   )
 }
 
